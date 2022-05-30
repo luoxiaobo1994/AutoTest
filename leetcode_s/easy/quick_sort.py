3b4267e27cb73e79c712bb1432979acc8af0515f
luoxiaobo <luoxiaobo@lxb.com>
2022-05-30 22:24:25 +0800

    基础类和算法

--- leetcode_s/easy/quick_sort.py
+++ leetcode_s/easy/quick_sort.py
@@ -8,7 +8,8 @@
 2.针对这个元素的左右侧,逐一递归步骤1.
 
 """
-
+import random
+from base.common import *
 
 def partition(ls, left, right):
     tmp = ls[left]
@@ -21,7 +22,20 @@ def partition(ls, left, right):
             left += 1
         ls[right] = ls[left]  # 把比归位数大的值,放到右边的空位上.
 
-    ls[left] = tmp
+    ls[left] = tmp  # 把归位值,放到应该在的位置.
+    return left  # 返回归位的下标
 
-def quick_sort():
-    pass
+
+def quick_sort(ls, left, right):
+    if left < right:
+        mid = partition(ls, left, right)
+        quick_sort(ls, left, mid - 1)
+        quick_sort(ls, mid + 1, right)
+
+
+ls = [5, 7, 4, 6, 3, 1, 2, 9, 8]
+l = list(range(1000000))
+random.shuffle(l)
+# partition(ls, 0, len(ls) - 1)
+quick_sort(l,0,len(ls)-1)
+print(l)
